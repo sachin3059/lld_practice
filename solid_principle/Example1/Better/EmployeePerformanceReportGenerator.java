e5689e1f819cf859f2853251f9de1779c58454d8
sachin3059 <sachin54077kumar@gmail.com>
2026-05-15 06:50:11 +0530

    added example of srp

---
+++ solid_principle/Example1/Better/EmployeePerformanceReportGenerator.java
@@ -0,0 +1,10 @@
+public EmployeePerformanceReportGenerator {
+    public void printPerformanceReport(Employee e){
+        // code to print performance report
+        System.out.println("Performance report of the employee: " + e.getEmployeeName());
+    }
+}
+
+
+
+// this class follows srp
---
+++ solid_principle/Example1/Better/EmployeeSalaryCalculator.java
@@ -0,0 +1,8 @@
+public class EmployeeSalaryCalculator{
+    public double computeSalary(Employee e){
+        // code to compute salary of the employee
+        return 10000.0;
+    }
+}
+
+// this class follows srp

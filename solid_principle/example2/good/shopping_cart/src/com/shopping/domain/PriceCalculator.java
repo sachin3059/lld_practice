9d03dad3e586d4765d96284fd6d31a34655ee79b
sachin3059 <sachin54077kumar@gmail.com>
2026-05-17 18:02:05 +0530

    implemented price calculator

---
+++ solid_principle/example2/good/shopping_cart/src/com/shopping/domain/PriceCalculator.java
@@ -0,0 +1,12 @@
+package solid_principle.example2.good.shopping_cart.src.com.shopping.domain;
+
+
+public class PriceCalculator {
+    public double calculateSubtotal(ShoppingCart cart){
+        if(cart == null){
+            throw new IllegalArgumentException("Cart cannot be null");
+        }
+
+        return cart.getItems().stream().mapToDouble(Item::getSubtotal).sum();
+    }
+}
